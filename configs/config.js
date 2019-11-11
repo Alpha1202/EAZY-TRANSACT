6c1bff85bdaf23b87df67ec6c455768443f9b083
Nzubechukwu <nzubennamani@gmail.com>
2019-11-11 17:18:17 +0100

    modified .env file modified .gitignore file add .sequelizerc file add sequelize migration files

--- configs/config.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-module.exports = {
-port: process.env.PORT || 2020,
-secret: process.env.SECRET_KEY
-}
---
+++ src/db/config/config.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DBNAME,
+    host: process.env.DATABASE_HOST,
+    dialect: 'postgres',
+    logging: false,
+  },
+  port: process.env.PORT || 2020,
+  secret: process.env.SECRET_KEY
+};
+
+
