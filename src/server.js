b237cdeff830c3ecf00612b2f92606707b17bd4a
Nzubechukwu <nzubennamani@gmail.com>
2019-11-12 03:04:51 +0100

    wrote api documentation using swagger

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import config from './db/config/config';
 import router from './routes';
+import swagger from 'swagger-ui-express';
+import swaggerConfig from '../swagger.json'
 
 const { port } = config
 
@@ -14,6 +16,8 @@ app.use(cors());
 // Normal express config defaults
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerConfig));
 
 // default home route
 app.get('/', (req, res) => {
